9ccdb3ec27c703e50ccd72a01722f296c31a10f9
Alex Corn <alex@alexcorn.com>
2020-09-30 05:11:09 -0400

    Added GitHub build action

--- HSPI_SharkRobot/Properties/AssemblyInfo.cs
+++ HSPI_SharkRobot/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HSPI_SharkRobot")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Alex Corn 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
